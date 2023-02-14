a873115b7f9f364dab282d12ac09aa27f731da47
shyam <shyamgupta0214@gmail.com>
2023-02-14 23:54:33 +0530

    added

--- src/component/WordBox.js
+++ src/component/WordBox.js
@@ -2,7 +2,15 @@
 
 function WordBox(){
   return(
+    <>
+   
    <input style={{width :500 ,height:30,borderRadius :10  }}type="text"></input>
+   <span class="material-symbols-outlined" style={{position :"absolute",left:"1075px",top:"420px"}}>mic </span>
+ 
+
+
+</>
+   
   )
 }
 
